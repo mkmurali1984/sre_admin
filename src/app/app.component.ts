3815791b21233c980362325404f7153fbf88b3dd
Murali <mkmurali.mca@gmail.com>
2023-06-25 17:08:47 +0530

    sre_admin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private http:HttpClient ){}
+
+  
   title = 'sre_admin';
+  isActive = true;
+  propertiesdata:any=[];
+
+  ngOnInit():void{
+    this.getPropertiesData();
+  }
+
+  getPropertiesData() {
+    this.http.get<any>(environment.API_URL+'Properties')
+    .subscribe(data => { 
+      this.propertiesdata = data;
+    });
+  }
+
+  clickUpdate(id:string){
+    console.log("PropertyID: " + id);
+    //console.log(this.propertiesdata);
+     this.http.put(environment.API_URL+"Properties", id ,this.propertiesdata)
+     .subscribe(res => { alert("Data Updated Successfully.")});
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production:true,
+    API_URL: "https://srewebapicore.azurewebsites.net/api/",
+    PHOTO_URL: "https://srewebapicore.azurewebsites.net/Photos/"
+};
